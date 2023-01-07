6fc84fa70ffb1102883914820fcc082aeb396da0
Isaac Diniz <isaacramonpb@gmail.com>
2023-01-07 19:46:38 -0300

    🚀 RELEASE: New style

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package yconmetrics
+package WeLog
 
 import (
 	"bytes"
@@ -7,89 +7,55 @@ import (
 	"net/http"
 )
 
-type Logger interface {
-	topic(string) *valueLogger
-	config(string, string, string, string, string) *logger
-	send() *printLogger
-	print()
+// WeLog é a estrutura da nossa biblioteca de log
+type WeLog struct {
+	APIURL      string
+	MachineID   string
+	Environment string
+	ServiceName string
 }
 
-type logger struct {
-	url         string
-	env         string
-	machineId   string
-	serviceName string
+// LogData é a estrutura dos dados do log que serão enviados para a API
+type LogData struct {
+	Topic   string      `json:"topic"`
+	Data    interface{} `json:"data"`
+	Machine string      `json:"machine"`
+	Env     string      `json:"env"`
+	Service string      `json:"service"`
 }
 
-func (l *logger) topic(topic string) *valueLogger {
-	return &valueLogger{
-		topic:  topic,
-		logger: l,
+// New cria uma nova instância da biblioteca de log
+func New(apiURL, machineID, environment, serviceName string) *WeLog {
+	return &WeLog{
+		APIURL:      apiURL,
+		MachineID:   machineID,
+		Environment: environment,
+		ServiceName: serviceName,
 	}
 }
 
-type valueLogger struct {
-	topic  string
-	value  string
-	logger *logger
-}
-
-func (vl *valueLogger) setvalue(value string) *printLogger {
-	return &printLogger{
-		topic:  vl.topic,
-		value:  value,
-		logger: vl.logger,
-	}
-}
-
-type printLogger struct {
-	topic  string
-	value  string
-	err    error
-	logger *logger
-}
-
-func (pl *printLogger) print() {
-	if pl.err != nil {
-		fmt.Println("Erro ao enviar log para o servidor:", pl.err)
-	} else {
-		fmt.Println(pl.topic, pl.value)
-	}
-}
-
-func (pl *printLogger) send() *printLogger {
-	logData := map[string]string{
-		"topic":       pl.topic,
-		"value":       pl.value,
-		"env":         pl.logger.env,
-		"machineid":   pl.logger.machineId,
-		"servicename": pl.logger.serviceName,
+// Topic envia um log para a API
+func (l *WeLog) Topic(topic string, data interface{}) error {
+	// Cria um novo objeto LogData com os dados do log
+	logData := &LogData{
+		Topic:   topic,
+		Data:    data,
+		Machine: l.MachineID,
+		Env:     l.Environment,
+		Service: l.ServiceName,
 	}
 
-	logDataBytes, err := json.Marshal(logData)
+	// Converte o objeto LogData para JSON
+	jsonData, err := json.Marshal(logData)
 	if err != nil {
-		pl.err = err
-		return pl
+		return fmt.Errorf("erro ao converter dados para JSON: %v", err)
 	}
 
-	resp, err := http.Post(pl.logger.url, "application/json", bytes.NewBuffer(logDataBytes))
+	// Envia a solicitação HTTP POST para a API
+	_, err = http.Post(l.APIURL, "application/json", bytes.NewBuffer(jsonData))
 	if err != nil {
-		pl.err = err
-		return pl
+		return fmt.Errorf("erro ao enviar solicitação HTTP POST: %v", err)
 	}
-	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		pl.err = fmt.Errorf("erro ao enviar log: código de status %d", resp.StatusCode)
-	}
-	return pl
-}
-
-func Config(url, env, machineId, serviceName string) *logger {
-	return &logger{
-		url:         url,
-		env:         env,
-		machineId:   machineId,
-		serviceName: serviceName,
-	}
+	return nil
 }
